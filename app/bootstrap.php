73b5d74f03e1a1f049ff32addb858eddfa0e9271
ShadeCormac <novemberrain0204@gmail.com>
2018-11-09 22:13:31 +0700

    adding base controller class

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,15 @@
 <?php
+    //Load config 
+    require_once "config/config.php";
+
     //Load lib's files
-    require_once "lib/Controller.php";
-    require_once "lib/Core.php";
-    require_once "lib/Database.php";
+    // require_once "lib/Controller.php";
+    // require_once "lib/Core.php";
+    // require_once "lib/Database.php";
+
+    //Autoload lib; replace the above "Load lib's file"
+    //more flexibility
+    spl_autoload_register(function($className) {
+        require_once 'lib/'.$className.'.php';
+    });
 ?>
---
+++ app/lib/controller.php
@@ -0,0 +1,23 @@
+<?php 
+    //Base controller
+    //Loads the models and views
+    class Controller {
+        //Load model
+        public function model($model){
+            //require the associtated model
+            require_once '../app/models/' .$model .'.php';
+
+            return new $model();
+        }
+
+        //Load View
+        public function view($view, $data = []){
+            if(file_exists('../app/views/'.$view.'.php')){
+                require_once('../app/views/'.$view.'.php');
+            }
+            else {
+                die('View died.');
+            }
+        }
+    }
+?>
