2b544c4120941ed045ef6bbaa94d7a9851345eaf
ShadeCormac <novemberrain0204@gmail.com>
2018-11-12 22:18:45 +0700

    add footer to page

--- app/views/include/footer.php
+++ app/views/include/footer.php
@@ -1,3 +1,83 @@
+<!-- ======================================================================
+                                        START FOOTER
+        ======================================================================= -->
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2 col-xs-6">
+                        <ul class="socials">
+                            <li><a href="#"><img src="<?=__URL__;?>/images/elements/socials/facebook.png" alt="facebook"/>Facebook</a></li>
+                            <li><a href="#"><img src="<?=__URL__;?>/images/elements/socials/twitter.png" alt="twitter"/>Twitter</a></li>
+                            <li><a href="#"><img src="<?=__URL__;?>/images/elements/socials/youtube.png" alt="youtube"/>Youtube</a></li>
+                            <li><a href="#"><img src="<?=__URL__;?>/images/elements/socials/googleplus.png" alt="google"/>Google+</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-xs-6">
+                        <ul>
+                            <li><a href="#">Register now</a></li>
+                            <li><a href="#">Subscribe for newsletter</a></li>
+                            <li><a href="#">Checkout</a></li>
+                            <li><a href="#">Delivery</a></li>
+                            <li><a href="#">Contact us</a></li>
+                            <li><a href="#">Faq</a></li>
+                            <li><a href="#">Ask us</a></li>
+                            <li><a href="#">Our partners</a></li>
+                            <li><a href="#">About us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-xs-6">
+                        <ul>
+                            <li><a href="#">Payments</a></li>
+                            <li><a href="#">Size guide</a></li>
+                            <li><a href="#">Order tracking</a></li>
+                            <li><a href="#">Order history</a></li>
+                            <li><a href="#">Returns</a></li>
+                            <li><a href="#">Terms & conditions</a></li>
+                            <li><a href="#">Security & privacy</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-xs-6">
+                        <ul class="menu-item">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Laptop</a></li>
+                            <li><a href="#">Mobile device</a></li>
+                            <li><a href="#">Headphone</a></li>
+                            <li><a href="#">Keyboard</a></li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="subscription">
+                            <div class="subscription-title">Newsletter subscribe</div>
+                            <form class="subscription" id="newsletter" method="post">
+                                <input type="text" name="newsletter-name" placeholder="Name" class="subscription-line">
+                                <input type="text" name="newsletter-email" placeholder="E-mail" class="subscription-line">
+                                <input type="submit" value="send" class="button-5">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="mini-footer">
+                    <div class="row">
+                        <div class="col-md-6">
+                            Copyright 2013 ZEON &nbsp;  &nbsp;  &nbsp; Designed by <a href="http://www.teslathemes.com">Teslathemes</a>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="text-right">
+                                <img src="<?=__URL__;?>/images/elements/payment.png" alt="payment systems" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ======================================================================
+                                        END FOOTER
+        ======================================================================= -->
+
 
         <script src="<?php echo __URL__; ?>/js/modernizr.custom.63321.js" type="text/javascript"></script>
         <script src="<?php echo __URL__; ?>/js/jquery-1.10.0.min.js" type="text/javascript"></script>
